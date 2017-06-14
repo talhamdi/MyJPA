0a9b4763c03156f2c72b93446a5297bb515ee733
t.aljehani <t.aljehani@IT-WS1.ogcy.com.sa>
2017-06-14 13:55:03 +0300

    14-06-2017

--- MyJPA/src/myjpa/MyJPA.java
+++ MyJPA/src/myjpa/MyJPA.java
@@ -1,9 +1,28 @@
 package myjpa;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class MyJPA {
 
     public static void main(String[] args) {
-        
+
+        EntityManagerFactory objFactory = Persistence.createEntityManagerFactory("MyJPAPU");
+        Student record = new Student();
+        StudentJpaController jpa = new StudentJpaController(objFactory);
+        List<Student> list = jpa.findStudentEntities();
+
+        record.setName("Test");
+
+        jpa.create(record);
+
+        for (Student l : list) {
+            System.out.println("Cod.: " + l.getId());
+        }
     }
-    
+
 }
+
+
